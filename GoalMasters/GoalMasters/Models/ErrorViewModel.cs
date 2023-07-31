98b35415c502d5c1f28d38d36b9b29f0ce057510
Gerardo Lanza <glanza007@gmail.com>
2023-07-31 15:13:19 -0600

    Create Project

---
+++ GoalMasters/GoalMasters/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoalMasters.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
